ac6bd6d7f98e8a4d11c0a030244c3eb6eecf0332
Marina <marinazelik6@gmail.com>
2021-03-14 22:06:56 +0300

    first

---
+++ 3.1/task1_for.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#define _USE_MATH_DEFINES
+
+double summ(int n)
+{ 
+    for (int i = 0; i < n; i++)
+        return pow((-1),i)*(i+1)/(pow(i,3)-(pow(i,2)+1)++summ;
+}
---
+++ 3.5/task5_do.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#define _USE_MATH_DEFINES
+
+double summ2(double eps)
+{ 
+    int i = 0;
+    do
+    {
+        return pow((-1),i)*(i+1)/(pow(i,3)-(pow(i,2)+1)+summ;
+        ++i;
+        if (fabs(summ) <= eps)
+        {    
+            continue;
+        }
+    }  while (i > 0);
+}
+
+
+
